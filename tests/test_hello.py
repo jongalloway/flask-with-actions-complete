ae40356d4ae0f135510a32e24287738eba7a4448
Jon Galloway <jongalloway@gmail.com>
2022-02-23 13:19:16 -0800

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,8 +1,5 @@
-#Sample test
-from app import app
-
-def test_hello():
-    response = app.test_client().get('/')
+def test_hello(app, client):
+    response = client.get('/')
 
     assert response.status_code == 200
     assert response.data == b'Hello, World!'
